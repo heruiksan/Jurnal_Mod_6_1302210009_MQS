f55a80acf9617889c0f8fed5338614743cd4a5db
heruiksan <heruiksan@gmail.com>
2023-03-29 17:17:19 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ Jurnal_Mod_6_1302210009_MQS/SayaTubeVideo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal_Mod_6_1302210009_MQS
+{
+    internal class SayaTubeVideo
+    {
+    }
+}
